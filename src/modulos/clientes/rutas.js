4223774accc699e041cb7f3d78952a50d89a8e31
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-12-15 18:57:13 -0300

    Suplanté en el cuerpo de la función dentro del get por el objeto que contiene el atributo con las funciones de las respuestas

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const respuesta = require('../../red/respuestas')
+
 const router = express.Router()
 
 router.get('/',(req,res) => {
-    res.send('Ruta Clientes OK')
+    respuesta.success(req, res, 'Todo OK desde Clientes', 200)
 });
 
 module.exports = router;
