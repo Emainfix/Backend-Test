0a5389235e2e54562f46acf25af56d3a94878892
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-12-16 16:45:05 -0300

    Hice la base de las funciones para el CRUD

---
+++ src/DB/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const config = require('../config');
+
+function todos(tabla){
+
+}
+
+function uno(tabla, id){
+
+}
+
+function agregar (){
+
+}
+
+function eliminar(tabla, id){
+
+}
+
+module.exports = {
+    todos,
+    uno,
+    agregar,
+    eliminar,
+}
