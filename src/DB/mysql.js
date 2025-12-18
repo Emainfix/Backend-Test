f39bbe5c793a99d94b7be4df4fcf258325b04f90
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-12-17 22:49:33 -0300

    Terminé de conectar mi base datos en workbench

--- src/DB/mysql.js
+++ src/DB/mysql.js
@@ -1,5 +1,38 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const config = require('../config');
+
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+}
+
+let conexion;
+
+function conMysql(){
+    conexion = mysql.createConnection(dbconfig);
+
+    conexion.connect((err)=>{
+        if(err){
+            console.log('db err', err);
+            //setTimeout(conMysql, 2000);
+        }else{
+            console.log('Conexión establecida exitosamente')
+        }
+    });
+
+    conexion.on('error',(err)=>{
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            conMysql();
+        }else{
+            throw err;
+        }
+    })
+}
+
+conMysql();
 
 function todos(tabla){
     return 'Todos - Acá estaría la información'
