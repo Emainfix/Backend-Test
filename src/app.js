8763eefb84d8fff7cbe19d356403f3ddf00d7f58
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-12-23 23:30:56 -0300

    Dupliqué las funcionalidades pero esta vez para manejar funcionalidades de usuarios

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const config = require('./config');
 
 const clientes = require('./modulos/clientes/rutas')
+const usuarios = require('./modulos/usuarios/rutas')
 const error = require('../src/red/errors')
 
 //Importado de la función objeto 
@@ -18,6 +19,7 @@ app.set('port', config.app.port)
 
 //Rutas
 app.use('/api/clientes', clientes)
+app.use('/api/usuarios', usuarios)
 app.use(error);
 
 module.exports = app;
