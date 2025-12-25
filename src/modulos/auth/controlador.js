fdc86a0b24561d217786fcb99a2419be6aab3869
Emanuel Villegas <fevillegas.dev@gmail.com>
2025-12-25 17:44:03 -0300

    Trabajé en la seguridad dividiendo la iformación del usuario en dos tablas en la base de datos y apliqué hash a los passwords

---
+++ src/modulos/auth/controlador.js
@@ -0,0 +1,32 @@
+const TABLA = 'auth';
+const bcrypt = require('bcrypt');
+
+module.exports = function(dbInyectada){
+
+    let db = dbInyectada;
+
+    if(!db){
+        db = require('../../DB/mysql');
+    }
+
+    async function agregar(data){
+
+        const authData = {
+            id: data.id
+        }
+
+        if(data.usuario){
+            authData.usuario = data.usuario;
+        }
+
+        if(data.password){
+            authData.password = await bcrypt.hash(data.password.toString(), 5); 
+        }
+
+        return db.agregar(TABLA, authData);
+    }
+
+    return{
+        agregar
+    }
+}
--- src/modulos/usuarios/controlador.js
+++ src/modulos/usuarios/controlador.js
@@ -1,4 +1,5 @@
 const TABLA = 'usuarios';
+const auth = require('../auth');
 
 module.exports = function(dbInyectada){
 
@@ -16,9 +17,37 @@ module.exports = function(dbInyectada){
         return db.uno(TABLA, id);
     }
 
-    function agregar(body){
-        return db.agregar(TABLA, body);
+    async function agregar(body){
+
+        const usuario = {
+            id: body.id,
+            nombre: body.nombre,
+            activo: body.activo
+        }
+
+        const respuesta = await db.agregar(TABLA, usuario);
+        console.log('respuesta', respuesta);
+
+        var numeroId = 0;
+
+        if(body.id == 0){
+            numeroId = respuesta.insertId;
+        }else{
+            numeroId = body.id;
+        }
+
+        var respuesta2 = '';
+        if(body.usuario || body.password){
+            respuesta2 = await auth.agregar({
+                id: numeroId,
+                usuario: body.usuario,
+                password: body.password
+            })
+        }
+
+        return respuesta2;
     }
+        
 
     function eliminar(id){
         return db.eliminar(TABLA, id);
